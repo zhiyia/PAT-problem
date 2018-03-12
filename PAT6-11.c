37a685516b3aafdf0aa370305742355d7ecd4269
zhiyia <894703504@qq.com>
2018-03-12 12:04:45 +0800

    sort
    shell's sort.I feell so difficult.

--- PAT6-11.c
+++ PAT6-11.c
@@ -19,18 +19,39 @@ int main()
 }
 
 //第一反应冒泡排序再取中值
+//ElementType Median(ElementType A[], int N) {
+//	int i,j,x;
+//	ElementType temp;
+//	for (i = 0; i < N; i++) {
+//		for (j = 0; j < i; j++) {
+//			if (A[i] < A[j]) {
+//				temp = A[i];
+//				A[i] = A[j];
+//				A[j] = temp;
+//			}
+//		}
+//	}
+//	x = N / 2;
+//	return A[x];
+//}
+//超时了。。。
+
+//think!优化算法。。。
+//怎么比较这个中值！
+//希尔排序...
 ElementType Median(ElementType A[], int N) {
-	int i,j,x;
+	int i,j,gap;
 	ElementType temp;
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < i; j++) {
-			if (A[i] < A[j]) {
-				temp = A[i];
-				A[i] = A[j];
-				A[j] = temp;
+	gap = N / 2;
+	for (gap; gap > 0;gap/=2) {
+		for (i = gap; i < N;i++) {
+			for (j = i - gap; j >= 0 && A[j] > A[j + gap]; j -= gap) {
+				temp = A[j];
+				A[j] = A[j+gap];
+				A[j+gap] = temp;
 			}
 		}
 	}
-	x = N / 2;
+	int x = N / 2;
 	return A[x];
 }
