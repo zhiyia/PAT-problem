313bca8d844b928ce1ee53cdad68219d234c4a36
zhiyia <894703504@qq.com>
2018-03-15 15:06:43 +0800

    ...

---
+++ PAT7-13.c
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+//找来找去发现一个单词打错了。。。。。无话可说
+int main() {
+	float Open, Close, High, Low;
+	int flag = 0;
+	scanf_s("%f %f %f %f", &Open, &High, &Low, &Close);
+	if (Open>Close) {
+		printf("BW-Solid");
+	}
+	else if (Open<Close) {
+		printf("R-Hollow");
+	}
+	else{
+		printf("R-Cross");
+	}
+	if (Low<Close&&Low<Open) {
+		flag =flag+ 1;
+	}
+	if (High>Open&&High>Close) {
+		flag =flag+ 2;
+	}
+	if (flag == 3) {
+		printf(" with Lower Shadow and Upper Shadow");
+	}
+	else if (flag == 1) {
+		printf(" with Lower Shadow");
+	}
+	else if (flag == 2) {
+		printf(" with Upper Shadow");
+	}
+	return 0;
+}
